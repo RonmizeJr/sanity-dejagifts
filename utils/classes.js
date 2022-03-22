4b80beee7e139ed10d32d353643a4c748a9af45c
Ronald Mize Jr <rwthojr5@gmail.com>
2022-03-22 18:24:18 -0500

    changes made

---
+++ utils/classes.js
@@ -0,0 +1,26 @@
+const classes = {
+  main: {
+    marginTop: 2,
+    minHeight: '80vh',
+  },
+  footer: {
+    marginTop: 1,
+    textAlign: 'center',
+  },
+  appbar: {
+    background: 'radial-gradient(#fff, #32CD32)',
+    '& a': {
+      color: '#000',
+      marginLeft: 1,
+    },
+  },
+  toolbar: {
+    justifyContent: 'space-between',
+  },
+  brand: {
+    fontWeight: 'bold',
+    fontSize: '1.5rem',
+  },
+};
+
+export default classes;
